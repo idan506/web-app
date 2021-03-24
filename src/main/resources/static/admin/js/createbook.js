25cf1994fa30a5f8e9828ff5e60692dfad964b5f
idan506 <58820156+idan506@users.noreply.github.com>
2021-03-24 17:31:12 +0700

    update

--- src/main/resources/static/admin/js/createbook.js
+++ src/main/resources/static/admin/js/createbook.js
@@ -1,24 +1,30 @@
 
+        let image ;
         $('#btnCreate').click(() => {
-            let dataToPost = {
-                name: document.getElementById("name").value,
-                isbn: document.getElementById("isbn").value,
-                publishing_year: document.getElementById("publishing_year").value,
-                author: document.getElementById("author").value,
-                shortDescription: document.getElementById("shortDescrpition").value,
-                status:true,
-                flag: true
-            };
-            console.log(dataToPost);
-            $.ajax({
-                url: '/api/books',
-                type: 'POST',
-                dataType: 'json',
-                contentType : 'application/json',
-                data: JSON.stringify(dataToPost),
-                success: (response) => {
-                    window.location='/admin/books-list.html';
-                }
-            });
+            console.log(image);
+            // let dataToPost = {
+            //     name: document.getElementById("name").value,
+            //     isbn: document.getElementById("isbn").value,
+            //     publishYear: document.getElementById("publishing_year").value,
+            //     author: document.getElementById("author").value,
+            //     shortDescription: document.getElementById("shortDescrpition").value,
+            //     quantity: document.getElementById("quantity").value,
+            //     status:true,
+            //     image: image,
+            //     flag: true
+            // };
+            // console.log(dataToPost);
+            // $.ajax({
+            //     url: '/api/books',
+            //     type: 'POST',
+            //     dataType: 'json',
+            //     contentType : 'application/json',
+            //     data: JSON.stringify(dataToPost),
+            //     success: (response) => {
+            //         window.location='/admin/books-list.html';
+            //     }
+            // });
 
         })
+
+      
