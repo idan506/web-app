042e1493e4d92eae85f94a16f54da50ed58932ab
khoa <anhkhoa561998@gmai.com>
2021-03-25 00:25:05 +0700

    update

--- src/main/resources/static/admin/js/books-list.js
+++ src/main/resources/static/admin/js/books-list.js
@@ -7,7 +7,7 @@
             $.each(response, function (i, item) {
                 trHTML += '<tr><td>' + item.id + '</td><td>' + item.name + '</td><td>' 
                     + item.isbn + '</td><td>' + item.publishYear + '</td><td style="font-size: small;">'  + item.shortDescription + '</td><td>' 
-                        + item.author + '</td><td>' + item.status +
+                        + item.author + '</td><td>' + item.status + '</td><td>' + item.quantity +
                          '</td><td>' + '<a class="btn btn-dark" href="./editbook.html?id='+item.id+'" >Edit</a>'+'&nbsp;'+
                              '<button class="btn btn-danger" onclick="deleteBtn('+item.id+')" id="btnDelete" value="'+item.id+'">Delete</button>' + '</td></tr>';
             });
